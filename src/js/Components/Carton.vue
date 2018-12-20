eafc8e1e86bbe59b0abe107530b50ff83532b0d4
Ozuna <oppaozuna@gmail.com>
2018-12-20 17:23:33 -0600

    Carton

--- src/js/Components/Carton.vue
+++ src/js/Components/Carton.vue
@@ -3,13 +3,23 @@
 		<main role="main" class="container sintesis">
 			<h1 class="mt-5">Carton</h1>
 			<p class="lead">
-				Cartones
+				Elija su contenido Cartones
 			</p>
+			<div>
+				<select class="image-picker select-cartoon show-html" multiple="multiple">
+					<option v-for="sakura in this.$root.cartoons" :key="sakura.idEditorial" :data-img-src="'assets/cuts/'+sakura.png" data-img-label="" :value="sakura.idEditorial" selected=true></option>
+				</select>
+			</div>
 		</main>
 	</div>
 </template>
 <script>
 	export default{ 
+		created(){
+			setTimeout(function(){
+			$("select.select-cartoon").imagepicker({show_label: false});
+					},1000);
+		},
 		methods:{
 			go: function(){
 				//pdfMake.createPdf(docDefinition).print();
